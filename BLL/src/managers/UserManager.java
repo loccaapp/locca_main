fd0108c7c62afd7e0682cf25a89050255dc782a2
gollumsss <uygarer@yandex.com>
2016-06-02 00:34:33 +0300

    UserManager error correction

--- BLL/src/managers/UserManager.java
+++ BLL/src/managers/UserManager.java
@@ -44,36 +44,69 @@ public class UserManager extends BaseManager {
 	
 	//added by ue 01.06.2016
 	public OperationResult getUser(int user_id){
-		try {
+		
+		int i = 0 ;
+		try {			
 			dbStatement = (Statement) dbConnection.createStatement();
-			dbResultSet = dbStatement.executeQuery("select * from tp_user where user_id = " + user_id + " ");		
+			i++;
+			
+			dbResultSet = dbStatement.executeQuery("select user_id, username, user_pwd, user_type, user_sub_type, name_first, name_last, email_address, picture_id, education, gender, motto, birthdate, phone_country_code, phone_operator_code, phone_num, status_id, account_try_count, last_activity_ts, create_ts, update_ts "
+					+ " from tp_user where user_id = " + user_id + " ");		
+			
+			i++;
 			
 			User user = new User();
+			while(dbResultSet.next()){
+			i++;
 			user.user_id = dbResultSet.getInt("user_id");
+			i++;
 			user.username = dbResultSet.getString("username");
+			i++;
 			user.user_pwd = dbResultSet.getString("user_pwd");
+			i++;
 			user.user_type = dbResultSet.getString("user_type");
+			i++;
 			user.user_sub_type = dbResultSet.getString("user_sub_type");
+			i++;
 			user.name_first = dbResultSet.getString("name_first");
+			i++;
 			user.name_last = dbResultSet.getString("name_last");	
+			i++;
 			user.email_address = dbResultSet.getString("email_address");
+			i++;
 			user.picture_id = dbResultSet.getString("picture_id");
+			i++;
 			user.education = dbResultSet.getString("education");
+			i++;
 			user.gender = dbResultSet.getString("gender");
+			i++;
 			user.motto = dbResultSet.getString("motto");	
+			i++;
 			user.birthdate = dbResultSet.getDate("birthdate");
+			i++;
 			user.phone_country_code = dbResultSet.getInt("phone_country_code");
+			}
 			
+			i++;
 			OperationResult result = new OperationResult();
+			i++;
 			result.isSuccess = true;
+			i++;
 			result.message = "Success";
+			i++;
 			result.object = user;
+			i++;
 			return result;
 			
 		} catch (SQLException e) {
 			OperationResult result = new OperationResult();
 			result.isSuccess = false;
-			result.message = "error1 : " + e.getMessage();
+			result.message = "iii: " + i + "User: " + user_id + " Error1 : " 
+					+ e.getStackTrace() + "=="
+					+ e.getMessage() + "--" 
+					+ e.getErrorCode()+ "**" 
+					+ e.getSQLState() + "++" 
+					+ e.getStackTrace();
 			return result;
 		}		
 	}
